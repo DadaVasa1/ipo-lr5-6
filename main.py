2671210bc8408cc249e726ef32ac69f362642da2
Дарья <hzxz4610@gmail.com>
2024-11-20 18:03:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ filename = 'text.txt'
 outputname = 'output.txt'
 
 with open(filename) as file, open(outputname, 'w') as output:
-  for i in file:
-    if len(i) > 5:  
-      output.write(i)
+    for i in file:
+        if len(i) > 5:  
+            output.write(i)
 
 print("готово")
